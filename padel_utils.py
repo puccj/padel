3bf02e9473e79fef7dd67af42f08ce58fed72d36
puccj <daniele.pucci2@studio.unibo.it>
2025-04-04 11:23:58 +0200

    Rename pose estimation to specify mediapipe

--- padel_utils.py
+++ padel_utils.py
@@ -116,7 +116,7 @@ def draw_bboxes(frame, player_dict, show_id = False):
         x1, y1, x2, y2 = bbox
         # bbox[0] = x_min     bbox[1] = y_min
         if show_id:
-            cv2.putText(frame, f"Player ID: {track_id}",(int(bbox[0]),int(bbox[1] -10 )),cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+            cv2.putText(frame, f"Player {track_id}",(int(bbox[0]),int(bbox[1] -10 )),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
         cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
     
     return frame
